9ee84b0bdabd859cca5cc978398f624b0d5a3551
sanjossye <sanjossye@gmail.com>
2025-10-31 13:26:34 +0700

    menambahkan page dan fitur absen coy

--- routes/web.php
+++ routes/web.php
@@ -3,37 +3,42 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\AbsensiController;
 
-// Route::get('/', function () {
-//     return Inertia::render('index', [
-//         'canRegister' => Features::enabled(Features::registration()),
-//     ]);
-// })->name('home');
 
 Route::get('/', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    
+
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
     
- Route::get('/detail-absen', function () {
-    return Inertia::render('detailpages/DetailAbsen');
-})->name('detail-absen');
+  
+
+    Route::get('/absensi/{id}', function ($id) {
+        return Inertia::render('user/absensi', ['id' => $id]);
+    })->name('user.absensi');
 });
 
-<<<<<<< HEAD
 Route::get('/admin/dashboard', function () {
     return Inertia::render('admin/Dashboard');
 })->name('admin.dashboard');
 
+Route::middleware(['auth'])->group(function () {
+    Route::post('/absensi/store', [AbsensiController::class, 'store'])->name('absensi.store');
+});
 require __DIR__.'/settings.php';
 
-Route::get('/absensi', function () {
-        return Inertia::render('user/absensi');
-    })->name('user.absensi');
-=======
-require __DIR__.'/settings.php';
->>>>>>> main
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/absensi/store', [AbsensiController::class, 'store'])->name('absensi.store');
+    Route::get('/absensi/riwayat', [AbsensiController::class, 'getRiwayat'])->name('absensi.riwayat');
+
+    Route::get('/absensi', function () {
+        return Inertia::render('CatatKehadiran'); 
+    })->name('absensi');
+});
